6a5698abef01613dae63d3ecea210b27a9c0ca68
이효규 <lhg961006@gmail.com>
2025-02-13 14:33:53 +0900

    레이아웃 수정.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,22 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "2025 타로 운세",
   description: "MZ세대를 위한 현대적인 타로 운세 웹사이트",
-    generator: 'v0.dev'
+  icons: {
+    icon: '/favicon.ico',
+  },
+  keywords: ['타로', '운세', '타로카드', '타로운세', '타로카드운세'],
+  openGraph: {
+    type: 'website',
+    title: '2025 타로 운세',
+    url: 'https://2025-tarot.vercel.app/',
+    description: 'MZ세대를 위한 현대적인 타로 운세 웹사이트',
+    locale: 'ko_KR',
+    images: [{
+      url: 'https://raw.githubusercontent.com/lhg1006/portfolio-images/623216ea41f64878a5b3e96730ced5e09345f4f9/images/project/tarot-0.png',
+      width: 1200,
+      height: 630,
+    }]
+  },
 }
 
 export default function RootLayout({

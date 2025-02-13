14a8a255c137263fd0ffa85695b44a4e356aabf7
이효규 <lhg961006@gmail.com>
2025-02-13 14:40:29 +0900

    타이틀 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,17 @@ import type React from "react"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "2025 타로 운세",
-  description: "MZ세대를 위한 현대적인 타로 운세 웹사이트",
+  title: "오늘의 타로",
+  description: "오늘의 타로 운세를 알려드립니다.",
   icons: {
     icon: '/favicon.ico',
   },
   keywords: ['타로', '운세', '타로카드', '타로운세', '타로카드운세'],
   openGraph: {
     type: 'website',
-    title: '2025 타로 운세',
+    title: '오늘의 타로',
     url: 'https://tarot-fortune.vercel.app/',
-    description: 'MZ세대를 위한 현대적인 타로 운세 웹사이트',
+    description: '오늘의 타로 운세를 알려드립니다.',
     locale: 'ko_KR',
     images: [{
       url: 'https://raw.githubusercontent.com/lhg1006/portfolio-images/623216ea41f64878a5b3e96730ced5e09345f4f9/images/project/tarot-0.png',
